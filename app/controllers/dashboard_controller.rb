59eedfc266776d9f00b2923a442152210c48ba76
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-05-03 12:16:51 +0100

    Set up .env and ruby deltek

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class DashboardController < ApplicationController
   def index
+    deltek = init_deltek
+    projects = deltek.projects
   end
+
+  private
+    def init_deltek
+      Deltek.new(Rails.application.secrets.deltek['wsdl'],
+                  Rails.application.secrets.deltek['username'],
+                  Rails.application.secrets.deltek['password'],
+                  Rails.application.secrets.deltek['database_name'])
+    end
 end
