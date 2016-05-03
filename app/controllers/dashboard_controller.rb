9f92b7a868099d6ae936dd64c33065157b2d03c7
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-05-03 12:49:32 +0100

    Updated deltek client

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,12 @@
 class DashboardController < ApplicationController
   def index
     deltek = init_deltek
-    projects = deltek.projects
+    projects = deltek.projects(10, 10)
   end
 
   private
     def init_deltek
-      Deltek.new(Rails.application.secrets.deltek['wsdl'],
+      Deltek.config(Rails.application.secrets.deltek['wsdl'],
                   Rails.application.secrets.deltek['username'],
                   Rails.application.secrets.deltek['password'],
                   Rails.application.secrets.deltek['database_name'])
