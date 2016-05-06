9da7bf354908e9ab445cf9d8c0799380a54bacb1
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-05-06 10:56:37 +0100

    Working proof of concept

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    deltek = init_deltek
-    projects = deltek.projects(10, 10)
+    @deltek = Deltek.projects(25)
   end
-
-  private
-    def init_deltek
-      Deltek.config(Rails.application.secrets.deltek['wsdl'],
-                  Rails.application.secrets.deltek['username'],
-                  Rails.application.secrets.deltek['password'],
-                  Rails.application.secrets.deltek['database_name'])
-    end
 end
---
+++ config/initializers/deltek.rb
@@ -0,0 +1,4 @@
+Deltek.config(Rails.application.secrets.deltek['wsdl'],
+              Rails.application.secrets.deltek['username'],
+              Rails.application.secrets.deltek['password'],
+              Rails.application.secrets.deltek['database_name'])
