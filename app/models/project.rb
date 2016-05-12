9bd4dd4ca9dac0be20366cf73f350c529c42ea39
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-05-12 12:28:20 +0100

    Added back progress bars, being calculated from local database

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,12 @@
 class Project < ActiveRecord::Base
+  def budget_remaining
+    budget = self.data["total_project_cost"].to_f
+    staff_cost = self.data["firm_cost"].to_f
+
+    budget - staff_cost
+  end
+
+  def percentage_budget_remaining
+    (self.budget_remaining / self.data["total_project_cost"].to_f * 100).round(2)
+  end
 end
